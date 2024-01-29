191a0a380eb2cfad4b60dfddef42c9239f48036f
Zebado <sbasualdo.antares@gmail.com>
2024-01-29 16:03:24 -0300

    cvbd

--- Assets/Scripts/AgentMove.cs
+++ Assets/Scripts/AgentMove.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AgentMove : MonoBehaviour
 {
-    Vector3 _targetPosition;
+    Vector3 _targetPosition = new Vector3();
     [SerializeField, Range(0, 10)] float _speed;
     SelectAgent _agentMove;
 
-    //objetivo : que detecte que personaje estoy clickeando y luego poder mover ese personaje y que el otro no se mueva
-    // - deberiamos tener un objeto padre donde almacenar el dato del personaje.
+    private void Start()
+    {
+        _targetPosition = new Vector3();
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
@@ -21,11 +23,18 @@ public class AgentMove : MonoBehaviour
             {
                 _targetPosition = hit.point;
 
-                Vector3 direction = _targetPosition - transform.position;
-                //_agentMove._agentSelected.transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-
             }
+        }
+        if (_targetPosition == null) return;
+        else
+        {
+            Vector3 direction = _targetPosition - transform.position;
+            transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+            if (transform.position == _targetPosition)
+            {
+                _targetPosition = Vector3.zero;
+            }
         }
 
     }
